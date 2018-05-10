930b87c33d19c9620fc7f93a8bf1ad9b82a006c1
zhenhailiu@sohu-inc.com <zhenhailiu@sohu-inc.com>
2018-05-10 12:18:09 +0800

    add setup.py for biterm package

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='biterm',
+      version='1.0',
+      description='biterm topic model',
+      author='liuzhenhai',
+      author_email='liuzhenhai93@outlook.com',
+      packages=['biterm'],
+      package_data={'biterm':["BTM.so"]}
+     )
